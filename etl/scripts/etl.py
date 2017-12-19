576c3240fa005be200e3548c93638b356fbde45b
semio <prairy.long@gmail.com>
2017-12-19 14:38:12 +0800

    update script and recipe

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -1,1 +1,20 @@
-print("nothing to do.")
+# -*- coding: utf-8 -*-
+
+import os
+
+from ddf_utils.chef.api import Chef
+
+
+out_dir = '../../'
+
+try:
+    datasets_dir = os.environ['DATASETS_DIR']
+except KeyError:
+    datasets_dir = '../../../'
+
+recipe_file = '../recipes/etl.yml'
+
+if __name__ == '__main__':
+    chef = Chef.from_recipe(recipe_file)
+    chef.add_config(ddf_dir=datasets_dir)
+    chef.run(serve=True, outpath=out_dir)
